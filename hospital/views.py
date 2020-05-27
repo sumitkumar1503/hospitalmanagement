248a5cc5c4aa99a15d3f19f4d328389a3bedde21
sumit <wapka1503@gmail.com>
2020-05-27 16:29:29 +0530

    send_mail is imported

--- hospital/views.py
+++ hospital/views.py
@@ -3,6 +3,7 @@ from . import forms,models
 from django.db.models import Sum
 from django.contrib.auth.models import Group
 from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required,user_passes_test
 from datetime import datetime,timedelta,date
 
