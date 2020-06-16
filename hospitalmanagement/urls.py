8ea7aab77eee54f5b9eb92ae8a26ecd3e3cc2d19
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2020-06-17 00:56:47 +0530

    Few bug fixes

--- hospitalmanagement/urls.py
+++ hospitalmanagement/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('adminsignup', views.admin_signup_view),
     path('doctorsignup', views.doctor_signup_view,name='doctorsignup'),
     path('patientsignup', views.patient_signup_view),
+    
     path('adminlogin', LoginView.as_view(template_name='hospital/adminlogin.html')),
     path('doctorlogin', LoginView.as_view(template_name='hospital/doctorlogin.html')),
     path('patientlogin', LoginView.as_view(template_name='hospital/patientlogin.html')),
