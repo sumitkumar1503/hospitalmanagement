afdf93b081261737fb0647531267195363218d0f
sumit <wapka1503@gmail.com>
2020-06-28 20:56:16 +0530

    removed fk

--- hospital/models.py
+++ hospital/models.py
@@ -34,7 +34,7 @@ class Patient(models.Model):
     address = models.CharField(max_length=40)
     mobile = models.CharField(max_length=20,null=False)
     symptoms = models.CharField(max_length=100,null=False)
-    assignedDoctorId = models.PositiveIntegerField(null=False)
+    assignedDoctorId = models.PositiveIntegerField(null=True)
     admitDate=models.DateField(auto_now=True)
     status=models.BooleanField(default=False)
     @property
@@ -48,8 +48,8 @@ class Patient(models.Model):
 
 
 class Appointment(models.Model):
-    patientId=models.PositiveIntegerField(null=False)
-    doctorId=models.PositiveIntegerField(null=False)
+    patientId=models.PositiveIntegerField(null=True)
+    doctorId=models.PositiveIntegerField(null=True)
     patientName=models.CharField(max_length=40,null=True)
     doctorName=models.CharField(max_length=40,null=True)
     appointmentDate=models.DateField(auto_now=True)
@@ -59,7 +59,7 @@ class Appointment(models.Model):
 
 
 class PatientDischargeDetails(models.Model):
-    patientId=models.PositiveIntegerField(null=False)
+    patientId=models.PositiveIntegerField(null=True)
     patientName=models.CharField(max_length=40)
     assignedDoctorName=models.CharField(max_length=40)
     address = models.CharField(max_length=40)
