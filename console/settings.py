1184270a259448fcffbb4d8b1e018a752431309e
zardian <amanb.singh21@gmail.com>
2021-12-11 23:57:38 +0530

    test3

--- console/settings.py
+++ console/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['hospital-appointment-system.azurewebsites.net']
 
 
 # Application definition
