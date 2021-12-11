99edc5653d371951942e16f4ce0f78c47b867a8d
zardian <amanb.singh21@gmail.com>
2021-12-11 19:50:58 +0530

    renamed

--- console/wsgi.py
+++ console/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hospitalmanagement project.
+WSGI config for project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospitalmanagement.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'console.settings')
 
 application = get_wsgi_application()
